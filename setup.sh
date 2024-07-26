85b7bab5990a8234cc1e4f58cd3aa7d92f74f212
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2024-07-26 12:52:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo '#Downloading source list and gpg key...'
-curl https://repo.fossfrog.in/fossfrog.gpg -o /etc/apt/trusted.gpg.d/fossfrog.gpg
-curl https://repo.fossfrog.in/fossfrog.list -o /etc/apt/sources.list.d/fossfrog.list
+curl -L https://repo.fossfrog.in/fossfrog.gpg -o /etc/apt/trusted.gpg.d/fossfrog.gpg
+curl -L https://repo.fossfrog.in/fossfrog.list -o /etc/apt/sources.list.d/fossfrog.list
 
 echo "#Updating apt cache..."
 apt update
