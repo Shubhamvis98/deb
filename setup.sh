4616c9936045d9e87564afbb0f9074e182eb1a96
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2024-11-03 18:32:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,13 @@ echo '#Downloading source list and gpg key...'
 curl -sL https://repo.fossfrog.in/fossfrog.gpg -o /etc/apt/trusted.gpg.d/fossfrog.gpg
 curl -sL https://repo.fossfrog.in/fossfrog.list -o /etc/apt/sources.list.d/fossfrog.list
 
+echo "#Adding apt preferences..."
+cat << EOF > /etc/apt/preferences.d/10-fossfrog-priority
+Package: *
+Pin: release o=fossfrog
+Pin-Priority: 1000
+EOF
+
 echo "#Updating apt cache..."
 apt update
 
