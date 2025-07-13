dcc857012f799b39cb780b6799cd969c7f74529f
Shubham Vishwakarma <shubhamvis98@fossfrog.in>
2025-07-13 17:35:05 +0530

    fix update with nhpro build script
    Signed-off-by: Shubham Vishwakarma <shubhamvis98@fossfrog.in>

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 echo '#Downloading source list and gpg key...'
 curl -sL https://repo.fossfrog.in/fossfrog.gpg -o /etc/apt/trusted.gpg.d/fossfrog.gpg
-curl -sL https://raw.githubusercontent.com/Shubhamvis98/deb/new/fossfrog.list -o /etc/apt/sources.list.d/fossfrog.list
+curl -sL https://repo.fossfrog.in/fossfrog.list -o /etc/apt/sources.list.d/fossfrog.list
 
 echo "#Adding apt preferences..."
 cat << EOF > /etc/apt/preferences.d/10-fossfrog-priority
